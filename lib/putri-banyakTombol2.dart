6d6857f893dd064a35492711c456baa4425755fa
regitamegad <megaloman104@gmail.com>
2020-12-01 21:03:46 +0700

    Update putri-banyakTombol2.dart
    update banyak tombol2 dan menambah toast message

--- lib/putri-banyakTombol2.dart
+++ lib/putri-banyakTombol2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,8 +9,18 @@ class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
+// class _ToastExample extends State{}
 
 class _MyAppState extends State<MyApp> {
+  void showToast() {
+    Fluttertoast.showToast(
+        msg: 'Button Pressed',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor: Colors.black87,
+        textColor: Colors.white);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,19 +37,19 @@ class _MyAppState extends State<MyApp> {
               children: <Widget>[
                 RaisedButton(
                   child: Text("Tombol 1"),
-                  onPressed: null,
+                  onPressed: showToast,
                 ),
                 RaisedButton(
                   child: Text("Tombol 2"),
-                  onPressed: null,
+                  onPressed: showToast,
                 ),
                 RaisedButton(
                   child: Text("Tombol 3"),
-                  onPressed: null,
+                  onPressed: showToast,
                 ),
                 RaisedButton(
                   child: Text("Tombol 4"),
-                  onPressed: null,
+                  onPressed: showToast,
                 ),
               ],
             ),
