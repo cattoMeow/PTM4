02f810ea0a6952905550fdeb49b82a7f2e1b95a2
Putri Anna <putrianna222000@gmail.com>
2020-12-01 07:44:18 +0700

    Create putri-navigasi.dart

---
+++ lib/putri-navigasi.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Objek Wisata',
+    home: Halaman1(),
+  ));
+}
+
+class Halaman1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Objek Wisata'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Selanjutnya'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Halaman2()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Halaman2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Objek Wisata 2"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+class Halaman3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Objek Wisata 2"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
