1fe5d5de65b8ab6e0d6d75edd1d03dace6a16866
Putri Anna <putrianna222000@gmail.com>
2020-12-01 07:24:51 +0700

    banyak tombol 2

---
+++ lib/putri-banyakTombol1.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("Banyak Tombol"),
+          ),
+        ),
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text("Tombol 1"),
+                  onPressed: null,
+                ),
+                RaisedButton(
+                  child: Text("Tombol 2"),
+                  onPressed: null,
+                ),
+                RaisedButton(
+                  child: Text("Tombol 3"),
+                  onPressed: null,
+                ),
+                RaisedButton(
+                  child: Text("Tombol 4"),
+                  onPressed: null,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
